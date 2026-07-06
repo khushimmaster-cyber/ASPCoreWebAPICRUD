49867e5c0974f7f17037e5f863f422d3445749fe
Dhyan Imvawala <your-email@gmail.com>
2026-07-06 11:29:48 +0530

    Add project files.

---
+++ Models/MydbContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPCoreWebAPICRUD.Models;
+
+public partial class MydbContext : DbContext
+{
+    public MydbContext()
+    {
+    }
+
+    public MydbContext(DbContextOptions<MydbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Student__3214EC07BF6939A6");
+
+            entity.ToTable("Student");
+
+            entity.Property(e => e.FatherName).HasMaxLength(100);
+            entity.Property(e => e.StudentGender).HasMaxLength(10);
+            entity.Property(e => e.StudentName).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
